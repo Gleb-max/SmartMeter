b9e9edb2c5cac8909cfac3236a6412d8e1d8954b
1MarBur1 <artpivko9@gmail.com>
2021-01-11 12:36:05 +0500

    feat: added button component

---
+++ SmartMeter/src/library/components/atoms/Button/Button.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TouchableOpacity, Text, View, ViewStyle, StyleProp } from 'react-native';
+
+//styles
+import styles from './Button.styles';
+
+//types
+type ButtonProps = {
+	onPress: () => void;
+	header: string;
+	style: StyleProp<ViewStyle>;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+	style,
+	onPress,
+	header,
+}) => {
+	return (
+		<TouchableOpacity
+			style= { [styles.container, style]}
+			onPress = { onPress }
+		>
+			<Text style={styles.text}>
+				{header}
+			</Text>
+		</TouchableOpacity>
+	);
+};
---
+++ SmartMeter/src/library/components/atoms/Button/Button.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		width: 214,
+		height: 44,
+		backgroundColor: '#005676',
+		borderRadius: 15,
+		elevation: 1,
+		justifyContent: 'center',
+	},
+	text: {
+		fontFamily: 'Roboto',
+		fontSize: 15,
+		alignSelf: 'center',
+		color: 'white',
+	},
+});
+
+export default styles;
