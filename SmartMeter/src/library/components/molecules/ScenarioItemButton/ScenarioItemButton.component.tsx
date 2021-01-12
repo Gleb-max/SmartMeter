307f8d320a7c186e60b6da6eb68ebb1faa4e5ee6
Gleb <gleb.demin.04@mail.ru>
2021-01-12 11:29:13 +0300

    feat: ScenarioItemButton component

---
+++ SmartMeter/src/library/components/molecules/ScenarioItemButton/ScenarioItemButton.component.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+
+//styles
+import styles from './ScenarioItemButton.styles';
+
+type ScenarioItemButtonProps = {
+    number: number;
+    text: string;
+    onPress?: () => void;
+};
+
+export const ScenarioItemButton: React.FC<ScenarioItemButtonProps> = ({
+    number,
+    text,
+    onPress,
+}) => {
+	return (
+        <TouchableOpacity style={styles.container} onPress={onPress}>
+            <View style={styles.numberContainer}>
+                <Text style={styles.number}>{number}</Text>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+	);
+};
